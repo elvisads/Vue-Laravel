ae1d8187e4a734eb3410cf01377158d3f2b21500
Elvis Gabriel Henrique <elvisprojetoss@gmail.com>
2020-11-08 17:33:50 -0300

    Loop's com blade

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -35,5 +35,23 @@
         @else
         <p>O nome não é Pedro</p>
         @endif
+
+        @for($i = 0; $i  < count($arr); $i++)
+            <p>{{ $arr[$i] }} - {{ $i }}</p>
+            @if ($i == 2)
+                <p>O i é 2</p>
+            @endif
+        @endfor
+
+        @foreach ($nomes as $nome)
+            <p>{{ $loop->index+1 }}</p>
+            <p>{{ $nome }}</p>
+        @endforeach
+
+
+        @php
+            $name = "João";
+            echo $name;
+        @endphp
     </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,18 @@ Route::get('/', function () {
     $idade = 29;
     $profissao = "Dev";
 
-    return view('welcome', ['nome' => $nome, 'idade' => $idade, 'profissao' => $profissao]);
+    $arr = [10,20,30,40,50];
+
+    $nomes = ["Matheus", "Maria", "João", "Saulo"];
+
+    return view('welcome',
+    [
+        'nome' => $nome,
+        'idade' => $idade,
+        'profissao' => $profissao,
+        'arr' => $arr,
+        'nomes' => $nomes
+    ]);
 });
 
 Route::get('/contact', function () {
