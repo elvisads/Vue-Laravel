cbadc2e09b3a5c724366b3580c141bb49b4fbacf
Elvis Gabriel Henrique <elvisprojetoss@gmail.com>
2020-11-08 20:09:40 -0300

    Criando layout com blade

--- resources/views/contact.blade.php
+++
@@ -1,4 +0,0 @@
-<h1>Esta é a pagina de contato</h1>
-<a href="/">Voltar pra home</a><br>
-<a href="/produtos">Ir para Produtos</a>
-
--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -1,1 +1,7 @@
+@extends('layouts.main')
+
+@section('title', 'Produtos')
+
+@section('content')
 <h1>Tela de produtos</h1>
+@endsection
