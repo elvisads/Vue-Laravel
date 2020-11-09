6d244b0706ac3c2547d705fc8461adb75fc6ad35
Elvis Gabriel Henrique <elvisprojetoss@gmail.com>
2020-11-09 16:59:18 -0300

    Resgatando parâmetros de URL

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -6,13 +6,50 @@
 
         <title>@yield('title')</title>
 
+        <!-- Fonte do Google -->
+        <link href="https://fonts.googleapis.com/css2?family=Roboto" rel="stylesheet">
+
+        <!-- CSS Bootstrap -->
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+
+
+        <!-- CSS da aplicação -->
         <link rel="stylesheet" href="/css/styles.css">
-        <script src="/js/script.js"></script>
+        <script src="/js/scripts.js"></script>
     </head>
     <body>
-    @yield('content')
-        <footer>
-            <p>HDC Events &copy; 2020</p>
-        </footer>
+      <header>
+        <nav class="navbar navbar-expand-lg navbar-light">
+          <div class="collapse navbar-collapse" id="navbar">
+            <a href="/" class="navbar-brand">
+              <img src="/img/hdcevents_logo.svg" alt="HDC Events">
+            </a>
+            <ul class="navbar-nav">
+              <li class="nav-item">
+                <a href="/" class="nav-link">Eventos</a>
+              </li>
+              <li class="nav-item">
+                <a href="/events/create" class="nav-link">Criar Eventos</a>
+              </li>
+              <li class="nav-item">
+                <a href="/" class="nav-link">Entrar</a>
+              </li>
+              <li class="nav-item">
+                <a href="/" class="nav-link">Cadastrar</a>
+              </li>
+            </ul>
+          </div>
+        </nav>
+      </header>
+
+
+      @yield('content')
+
+
+
+      <footer>
+        <p>HDC Events &copy; 2020</p>
+      </footer>
+      <script src="https://unpkg.com/ionicons@5.1.2/dist/ionicons.js"></script>
     </body>
 </html>
---
+++ resources/views/product.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.main')
+
+@section('title', 'Produto')
+
+@section('content')
+    @if ($id != null)
+    <p>Exibindo produto id: {{ $id }}</p>
+    @endif
+@endsection
